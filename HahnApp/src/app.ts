8988c2c321ad85e3046b5084900bda483701db2b
Nischal Subedi <subedi275@gmail.com>
2020-05-30 10:51:08 +0545

    Task #4 form implementation

--- HahnApp/src/app.ts
+++ HahnApp/src/app.ts
@@ -1,3 +1,42 @@
+// export class App {
+//   public message: string = 'Hello World!';
+// }
+
+interface Applicant {
+  
+  ID: number;
+  Name: string;
+  FamilyName: string;
+  Address: string;
+  CountryOfOrigin: string;
+  EmailAddress: string;
+  Age: number;
+  Hired: boolean;
+}
+
 export class App {
-  public message: string = 'Hello World!';
+  heading = "HahnApp";
+  applicants: Applicant[] = [];
+  formApplicant:Applicant;
+
+  send_onclick() {
+    console.log(this.formApplicant);
+    //check if all fields are entered
+
+    //push to list 
+    //this.applicants.push(this.formApplicant);
+
+    //reset all fields
+  }
+
+  reset_onclick(){
+    console.log("reset clicked");
+  }
+
+  removeApplicant(applicant) {
+    let index = this.applicants.indexOf(applicant);
+    if (index !== -1) {
+      this.applicants.splice(index, 1);
+    }
+  }
 }
